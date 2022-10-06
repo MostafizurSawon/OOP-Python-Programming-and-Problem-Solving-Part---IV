dafd93813602d9e8865774c9e68b47da7b63bc5d
unknown <mostafizur1102@gmail.com>
2022-10-06 20:44:00 +0600

    extra solution added

--- 1-week/3-lab-01/11.py
+++ 1-week/3-lab-01/11.py
@@ -6,7 +6,29 @@ Given:
 str_x = "Emma is good developer. Emma is a writer"
 Expected Output:
 
-Emma appeared 2 times """
+Emma appeared 2 times 
+
+solution : 1
+str_x = "Emma is good developer. Emma is a writer"
+# use count method of a str class
+cnt = str_x.count("Emma")
+print(cnt)
+
+
+solution : 2
+def count_emma(statement):
+    print("Given String: ", statement)
+    count = 0
+    for i in range(len(statement) - 1):
+        count += statement[i: i + 4] == 'Emma'
+    return count
+
+count = count_emma("Emma is good developer. Emma is a writer")
+print("Emma appeared ", count, "times")
+
+
+
+"""
 
 
 
@@ -31,6 +53,6 @@ def return_count(str):
 str = "Emma is good developer. Emma is a writer"
 # print(str)
 
-print(return_count(str))
+print("Emma appeared",return_count(str),"times")
 
 
