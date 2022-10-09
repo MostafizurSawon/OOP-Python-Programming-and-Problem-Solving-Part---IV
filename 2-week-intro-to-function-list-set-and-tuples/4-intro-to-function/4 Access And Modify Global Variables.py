bb97eee9dc018b5d56cb6d927c45a4c303852282
unknown <mostafizur1102@gmail.com>
2022-10-09 20:55:43 +0600

    global variables added

---
+++ 2-week-intro-to-function-list-set-and-tuples/4-intro-to-function/4 Access And Modify Global Variables.py
@@ -0,0 +1,15 @@
+balance = 5000
+
+def total_cost(price, quantity):
+    global balance
+    cost = price * quantity
+    remaining = balance - cost
+    balance = remaining   
+    #print(remaining)
+    return cost
+print("outside balance before: ",balance)
+# print(cost)       will not work
+pay = total_cost(30, 5)
+print(f' Please pay: {pay} taka')
+
+print("outside balance after: ",balance)
